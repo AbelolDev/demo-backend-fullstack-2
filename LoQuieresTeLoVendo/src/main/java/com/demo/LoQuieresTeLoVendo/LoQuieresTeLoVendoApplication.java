5f93365494117da9de808ee8f1250fb6d2cb0793
AbelolDev <155784727+AbelolDev@users.noreply.github.com>
2025-11-17 20:42:09 -0300

    Add files via upload

---
+++ LoQuieresTeLoVendo/src/main/java/com/demo/LoQuieresTeLoVendo/LoQuieresTeLoVendoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.LoQuieresTeLoVendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoQuieresTeLoVendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoQuieresTeLoVendoApplication.class, args);
+	}
+
+}
---
+++ LoQuieresTeLoVendo/src/test/java/com/demo/LoQuieresTeLoVendo/LoQuieresTeLoVendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.LoQuieresTeLoVendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoQuieresTeLoVendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
